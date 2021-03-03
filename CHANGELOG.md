f37df7fe2f7d3a5d8bcb0b599f31d200f144e97a
Kevin Bai <kevin.c.bai0@gmail.com>
2021-03-02 23:55:01 -0500

    add publish script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 # typescript-codegen
 
+## 0.0.3
+
+### Patch Changes
+
+- Test publish
+
 ## 0.0.2
+
 ### Patch Changes
 
 - Release first package
---
+++ scripts/publish.sh
@@ -0,0 +1,13 @@
+REMOTE_VERSION=$(npm info tscgen version)
+LOCAL_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+if [[ $REMOTE_VERSION == $LOCAL_VERSION ]] ; then
+  echo "Already latest version!"
+else
+  npm publish
+fi
